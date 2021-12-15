9db312fbe1f9a25198847ca2f291cebdd8b77858
Adria Pujol <adriapujolaparicio@gmail.com>
2021-12-15 19:31:30 +0000

    clean up and samesite none secure true

--- routes/users.js
+++ routes/users.js
@@ -6,7 +6,6 @@ const { authUser, authRole } = require('../basicAuth');
 
 
 router.get('/', verifyToken, authUser, authRole("admin"), async (req, res) => {
-    console.log(req.user);
     try {
         const users = await User.find({}, { password: 0 });
         res.status(200).send(users);
@@ -57,7 +56,6 @@ router.put('/form/:id', async (req, res) => {
 
 router.put('/isComing/:id', async (req, res) => {
     try {
-        console.log(req.body.isComing);
         await User.findByIdAndUpdate(req.params.id, { isComing: req.body.isComing }, { useFindAndModify: false });
         res.status(200).json("We got it");
     } catch (err) {
@@ -65,19 +63,6 @@ router.put('/isComing/:id', async (req, res) => {
     }
 });
 
-// router.delete('/delete/:id', async (req, res) => {
-//     const id = req.params.id;
-//     try {
-//         if (req.body.role === "admin") {
-//             await User.findByIdAndDelete(id);
-//             res.status(200).send('User Deleted');
-//         } else {
-//             res.status(401).json("You don't have clearance to do that");
-//         }
-//     } catch (err) {
-//         res.status(500).send(`There was and error deleting the user with id: ${id}`);
-//     }
-// })
 
 
 module.exports = router;
